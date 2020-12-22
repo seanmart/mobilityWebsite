b993cbf18a83cca4828766d738426085f6bf4705
Sean Metcalf <sean@seanmart.com>
2020-12-22 09:57:45 -0500

    added other pages

---
+++ pages/news.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <main>
+    news
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ pages/services.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <main>
+    services
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
